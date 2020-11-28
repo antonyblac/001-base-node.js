7317fc57b135a068fbabadd3fa171f109fd99b26
lorenzo.zambrano <tza5916@gmail.com>
2020-11-27 23:24:55 -0600

    Se agrego un arhivo  de informacion del proyecto

--- app.js
+++ app.js
@@ -1,25 +1,54 @@
 const argv = require('yargs')
     .command('listar', 'imprime en pantalla la tabla de multiplicar', {
-        base: {
-            demand: true
-            alias: ''
-
+        base:{
+            demand: true,
+            alias: 'b'
         },
-
-        limite: {
-            alias:''
+        limite:{
+            alias: 'l',
             default: 10
-
         }
-    })
 
+
+    })
+    .command('crear', 'Crea un archivo con la tabla de multiplicar', {
+        base:{
+            demand: true,
+            alias: 'b'
+        },
+        limite:{
+            alias: 'l',
+            default: 10
+        }
+
+
+    })
+    .help()
     .argv;
+const yargs = require('yargs');
 const multiplicar = require('./multiplicacion/multiplicacion');
 
-console.log('la base insertada fue:', argv.base);
-console.log('el limite insertada fue:', argv.limite);
+let comando = argv._[0];
+let base = argv.base;
+let limite = argv.limite;
 
-//multiplicar
-//.crearArchivo()
-//.then(archivo => console.log(`el archivo ${archivo} ha sido generado con exito`))
-//.catch(error  => console.log(`ocurrio un error ${error}`));
+switch(comando) {
+    
+    case `listar`:
+    
+    multiplicar.listarTabla(base, limite)
+    .then ()
+    .catch(error => console.log(`Ocurrio un error ${error}`));
+    break;
+    
+    case `crear`:
+    
+    multiplicar
+        .crearArchivo(base, limite)
+    .then(archivo => console.log(`El archivo ${archivo} ha sido generado con exito`))
+    .catch(error => console.log(`Ocurrio un error ${error}`));
+   
+    break;
+    default:
+    console.log(`Comando no reconocido`);
+}
---
+++ info.md
@@ -0,0 +1,4 @@
+## Instrucciones 
+-instalar node 
+-programar el codigo 
+-correr el proyecto con nodemon
